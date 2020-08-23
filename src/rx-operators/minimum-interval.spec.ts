594fc0316a3b72a6d111a5c78fc3a8e439e55f66
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2020-08-23 22:57:09 +0900

    Revert "feat: add repeatWithin operator function"
    This reverts commit 92033b37

--- src/rx-operators/minimum-interval.spec.ts
+++ src/rx-operators/minimum-interval.spec.ts
@@ -1,7 +1,7 @@
 import { TestScheduler } from 'rxjs/testing';
-import { minInterval } from './min-interval';
+import { minimumInterval } from './minimum-interval';
 
-describe('minInterval', () => {
+describe('waitIfProcessTimeIsUnder', () => {
   let scheduler: TestScheduler;
 
   beforeEach(() => {
@@ -14,13 +14,13 @@ describe('minInterval', () => {
     scheduler.run((helpers) => {
       const { cold, expectObservable } = helpers;
 
-      const stream = cold('400ms (a|)');
+      const stream = cold('400ms (a|)', { a: 'value' });
       const expected = '1000ms (a|)';
 
-      expectObservable(stream.pipe(minInterval(1000, scheduler))).toBe(expected, {
+      expectObservable(stream.pipe(minimumInterval(1000, scheduler))).toBe(expected, {
         a: {
-          data: 'a',
-          executionTime: 400,
+          data: 'value',
+          processTime: 400,
           waitTime: 600,
         },
       });
@@ -31,13 +31,13 @@ describe('minInterval', () => {
     scheduler.run((helpers) => {
       const { cold, expectObservable } = helpers;
 
-      const stream = cold('400ms (a|)');
+      const stream = cold('400ms (a|)', { a: 'value' });
       const expected = '400ms (a|)';
 
-      expectObservable(stream.pipe(minInterval(200, scheduler))).toBe(expected, {
+      expectObservable(stream.pipe(minimumInterval(200, scheduler))).toBe(expected, {
         a: {
-          data: 'a',
-          executionTime: 400,
+          data: 'value',
+          processTime: 400,
           waitTime: 0,
         },
       });
--- src/rx-operators/minimum-interval.ts
+++ src/rx-operators/minimum-interval.ts
@@ -1,7 +1,7 @@
 import { async, Observable, timer } from 'rxjs';
 import { delayWhen, map, timeInterval } from 'rxjs/operators';
 
-export const minInterval = (minimumProcessTime: number, scheduler = async) => {
+export const minimumInterval = (minimumProcessTime: number, scheduler = async) => {
   return (source: Observable<any>) => {
     let waitTime: number;
 
@@ -15,7 +15,7 @@ export const minInterval = (minimumProcessTime: number, scheduler = async) => {
       }),
       map((time) => ({
         data: time.value,
-        executionTime: time.interval,
+        processTime: time.interval,
         waitTime,
       })),
     );
